e5d7e965dc4ee2280101863fecbff55b4296be12
jessgrinberg <jessgrinberg@gmail.com>
2018-07-18 17:01:26 -0700

    added section carousel testimonials

--- js/app.js
+++ js/app.js
@@ -51,3 +51,26 @@ $( document ).ready(function() {
         showPage();
     });
 });
+
+
+
+$('#carouselTemoignage').on('slide.bs.carousel', function (e) {
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $('#carouselTemoignage .carousel-item').length;
+
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('#carouselTemoignage .carousel-item').eq(i).appendTo('#carouselTemoignage .carousel-inner');
+            }
+            else {
+                $('#carouselTemoignage .carousel-item').eq(0).appendTo('#carouselTemoignage .carousel-inner');
+            }
+        }
+    }
+});
